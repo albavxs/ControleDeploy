b43278a32a8670c1a69d63a4f651b92853e478bf
albavxs <paulogui5433@outlook.com>
2024-11-19 16:45:54 -0300

    alterando lógica de requisição

--- src/config_axios.js
+++ src/config_axios.js
@@ -1,3 +1,3 @@
-import axios from 'axios';
+import axios from "axios";
 // cria uma instance Axios com a URL base do Web Service a ser acessado pelo app
-export const inAxios = axios.create({baseURL: 'http://localhost:3001/'});
+export const inAxios = axios.create({ baseURL: "http://172.18.0.2:3001/" });
